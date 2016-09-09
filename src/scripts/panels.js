aa048655b9311b2cffb73878bfe0e580a255558d
Mark Anderson <mark.anderson@helpfulhuman.com>
2016-09-09 09:37:15 -0700

    added animation to the about us section

--- src/scripts/panels.js
+++ src/scripts/panels.js
@@ -6,12 +6,25 @@ export default function ($) {
 
   var currentStep = 1;
 
+  var currentStory = 1;
+
   $splash.addClass('is-step-' + currentStep);
 
   $(document).on('setpanel', function (ev, index) {
     $splash
       .removeClass('is-step-' + currentStep)
       .addClass('is-step-' + (currentStep = index));
+      console.log(ev, index);
+  });
+
+  $(document).on('setstory', function (ev, index) {
+    var story = 9;
+    if (index <=9) {
+      story = index
+    }
+    $splash
+      .removeClass('is-story-' + currentStory)
+      .addClass('is-story-' + (currentStory = story));
       console.log(ev, index);
   });
 
