d06c072092139af0297e38ad1b04944e21efe91e
zeekay <zk@monoid.io>
2016-09-16 21:11:46 -0500

    Clean up account JS.

--- js/account.js
+++ js/account.js
@@ -1,28 +1,35 @@
-$(function () {
+$(function() {
   var m = window.m;
 
   var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-  setupReferral = function(url) {
+  var displayCopied = function() {
+    setTimeout(function() {
+      $('.copied').fadeIn()
+      setTimeout(function() {
+        $('.copied').fadeOut()
+      }, 1000)
+    }, 250)
+  }
+
+  // Copy link to clipboard and fade in message on click
+  var setupClipboard = function() {
+    new Clipboard('.ref-link', {
+      target: function(trigger) {
+        if (!isSafari) {
+          displayCopied()
+        }
+        return $('.ref-link')[0]
+      }
+    });
+  }
+
+  var setupReferral = function(url) {
     $('.ref-text').html(url)
-    uri = encodeURIComponent(url)
+    var uri = encodeURIComponent(url)
     $('#fbLink').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + uri)
     $('#twLink').attr('href', 'https://twitter.com/intent/tweet?url=' + uri + '&amp;via=ludela')
-
-    // Copy link to clipboard and fade in message on click
-    if (!isSafari) {
-      new Clipboard('.ref-link', {
-          target: function(trigger) {
-            setTimeout(function() {
-              $('.copied').fadeIn()
-              setTimeout(function() {
-                $('.copied').fadeOut()
-              }, 1000)
-            }, 250)
-            return $('.ref-link')[0]
-          }
-      });
-    }
+    setupClipboard()
   }
 
   if (!window.client.client.customerToken) {
@@ -37,8 +44,8 @@ $(function () {
   // m.on('profile-load', function(){
   // });
 
-  m.on('profile-load-success', function(data){
-    setupReferral('https://ludela.com/#' +data.referrers[0].id);
+  m.on('profile-load-success', function(data) {
+    setupReferral('https://ludela.com/#' + data.referrers[0].id);
 
     if (data.firstName != '') {
       $('.important').hide();
@@ -51,20 +58,20 @@ $(function () {
 
     $('.friends-count').html(referrals);
 
-    if (referrals >= 5 ) {
+    if (referrals >= 5) {
       $('.point5').addClass('done').removeClass('todo');
     }
-    if (referrals >= 10 ) {
+    if (referrals >= 10) {
       $('.point10').addClass('done').removeClass('todo');
     }
-    if (referrals >= 25 ) {
+    if (referrals >= 25) {
       $('.point25').addClass('done').removeClass('todo');
     }
-    if (referrals >= 50 ) {
+    if (referrals >= 50) {
       $('.point50').addClass('done').removeClass('todo');
     }
 
-    for (var i = 0; i < Math.min(10, referrals); i++){
+    for (var i = 0; i < Math.min(10, referrals); i++) {
       var $li = $('<li>' + data.referrals[referrals - 1 - i].firstName + '</li>');
       $('.friend-names').append($li);
     }
