01221ac40364adf1e9cf250dbfc1089d17391e73
David Tai <david@verus.io>
2016-09-23 20:11:23 -0500

    the phone makes it all better

--- src/js/index.js
+++ src/js/index.js
@@ -60,6 +60,26 @@ jQuery(function ($) {
     }
   })
 
+  var imageRatio = 229/368
+  var updateAppSlider = function(){
+    var $phone = $('.visuals__phone')
+
+    var width = $phone.width()
+    var height = $phone.height()
+    var screenRatio = width / height
+
+    if (screenRatio > imageRatio) {
+      $('.app-slider').width(height * imageRatio)
+      $('.app-slider').height(height)
+    } else {
+      $('.app-slider').width(width)
+      $('.app-slider').height(width / imageRatio)
+    }
+  }
+
+  updateAppSlider()
+  $(window).on('resize', updateAppSlider)
+
   $.scrollify({
     section : ".splash__content",
     sectionName : "step",
