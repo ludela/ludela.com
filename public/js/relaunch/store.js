93b5c22cbaf8c8ce48aeb9cf265adb9ae7767856
David Tai <david@verus.io>
2016-10-11 13:57:25 -0500

    color picker

--- public/js/relaunch/store.js
+++ public/js/relaunch/store.js
@@ -10,4 +10,15 @@ $(window).load(function(){
   })
 
   instance.update().check().handlers(true);
+
+  $('.color-picker label').on('click', function(){
+    var $label = $(this);
+    var $color = $label.closest('span');
+    var color = $color[0]
+    var i = $color.index();
+
+    var $imgs = $color.closest('.item').find('.image img')
+    $imgs.removeClass('active');
+    $($imgs[i]).addClass('active');
+  })
 })
